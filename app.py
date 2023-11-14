aab504433176bc69eb24d0f1b11c672965123009
nextgen-user <150797204+nextgen-user@users.noreply.github.com>
2023-11-14 16:23:34 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, url_for, redirect
+import subprocess
+import os
+
+data={}
+app = Flask(__name__)
+
+@app.route("/execute", methods=["POST"])
+def execute():
+    global data
+    code = request.data.decode("utf-8")
+    print(code)
+    try:
+        exec(code,globals())  
+    except Exception as e:
+        print(e)
+        return {"Error":str(e)}
+    return data
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
