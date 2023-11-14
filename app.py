8f2cddd5eb3320e0d9ffb3ebdcfd48afd3011076
nextgen-user <150797204+nextgen-user@users.noreply.github.com>
2023-11-14 23:46:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,32 @@ import subprocess
 from flask import Flask, request, url_for, redirect
 from werkzeug.utils import secure_filename
 import os
- 
+import asyncio
+import random
+from duckduckgo_search import AsyncDDGS
+import urllib.request
 
+imgur=[]
 
+def getimage(query):
+
+    async def get_results():
+        global imgur
+        async with AsyncDDGS() as ddgs:
+            async for r in ddgs.images(query, max_results=20):
+                imgur.append(r["image"])
+
+    asyncio.run(get_results())
+    print(imgur)
+    for i in range(1,10):
+        try:
+            filename=f"static/image{random.randint(1,1000)}.jpg"
+            urllib.request.urlretrieve(random.choice(imgur), filename)
+            print(filename)
+            break
+        except:
+            pass
+    return filename
 
 
 # Files stored in
